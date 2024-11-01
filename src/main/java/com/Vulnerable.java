8708efdc7855b9a238f3642354fd9e1ae2a2fdbe
Aditya Gupta <adgupta1003@gmail.com>
2024-10-31 20:45:16 -0400

    Initialized project and git repository.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

---
+++ src/main/java/com/Vulnerable.java
@@ -0,0 +1,9 @@
+// src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class Vulnerable {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
